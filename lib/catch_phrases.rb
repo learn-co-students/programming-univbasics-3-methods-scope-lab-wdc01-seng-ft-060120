26d31112b1a844a94c744e1b336de5804ec1d4a8
Diego Zegarra <diego.zegarra79@icloud.com>
2020-04-20 16:40:24 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,9 +9,13 @@ def toadstool
  status = "Thank You Mario! But Our Princess Is In Another Castle!" 
   puts status
   end
+
+def link 
+  sentence = "It's Dangerous To Go Alone! Take This."
+  puts sentence
+end
   
   def all_phrases 
-    link = "It's Dangerous To Go Alone! Take This."
     puts mario 
     puts toadstool
     puts link
